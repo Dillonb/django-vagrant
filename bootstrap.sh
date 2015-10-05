b99097fcbd81d3bf26e3befa6fd4eb227b8598cd
Dillon Beliveau <dillon@greenmountainsoftware.com>
2015-10-05 15:21:59 -0400

    Updated to use pip and requirements.txt

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 apt-get update
 apt-get upgrade -y
-apt-get install -y python-django python-django-auth-ldap ruby-sass ruby-listen apache2
+apt-get install -y ruby-sass ruby-listen apache2 python-pip
+
+pip install -r /home/vagrant/app/requirements.txt
 
 mkdir -p /var/www/html/static/
